888384d5690f922ce7951aaf6bbf02ab5f2886b5
AbdulAzeem9 <162511682+AbdulAzeem9@users.noreply.github.com>
2024-06-27 14:53:28 +0500

    Add files via upload

---
+++ Class 16 OOP's/main.js
@@ -0,0 +1,46 @@
+"use strict";
+// Object Oriented Programming
+// type CarType = {
+//     brand: string
+//     model: string
+//     year: number
+// }
+// const myCar: CarType = {
+//     brand: "Toyota",
+//     model: "Corolla",
+//     year: 2024,
+// }
+// console.log(myCar);
+// interface CarType { // interface are especially for objects
+//   brand: string;
+//   model: string;
+//   year: number;
+// };
+// class Car implements CarType {
+//   brand: string = "Toyota";
+//   model: string = "Corolla";
+//   year: number = 2024;
+// }
+// let myCar = new Car();
+// console.log(myCar);
+// const myCar = {
+//   brand: "Toyota",
+//   model: "Corolla",
+//   year: 2024,
+//   displayDetails: function () {
+//     return `Brand ${myCar.brand} Model ${myCar.model} Year ${myCar.year}`;
+//   },
+// };
+// console.log(myCar.displayDetails());
+class Car {
+    constructor() {
+        this.brand = "Toyota";
+        this.model = "Corolla";
+        this.year = 2024;
+    }
+    displayDetails() {
+        return `Brand ${this.brand} Model ${this.model} Year ${this.year}`;
+    }
+}
+let myCar = new Car();
+console.log(myCar.displayDetails());
---
+++ Class 16 OOP's/main.ts
@@ -0,0 +1,48 @@
+// Object Oriented Programming
+
+// type CarType = {
+//     brand: string
+//     model: string
+//     year: number
+// }
+// const myCar: CarType = {
+//     brand: "Toyota",
+//     model: "Corolla",
+//     year: 2024,
+// }
+// console.log(myCar);
+
+// interface CarType { // interface are especially for objects
+//   brand: string;
+//   model: string;
+//   year: number;
+// };
+// class Car implements CarType {
+//   brand: string = "Toyota";
+//   model: string = "Corolla";
+//   year: number = 2024;
+// }
+// let myCar = new Car();
+// console.log(myCar);
+
+// const myCar = {
+//   brand: "Toyota",
+//   model: "Corolla",
+//   year: 2024,
+//   displayDetails: function () {
+//     return `Brand ${myCar.brand} Model ${myCar.model} Year ${myCar.year}`;
+//   },
+// };
+// console.log(myCar.displayDetails());
+
+// class Car {
+//   public brand = "Toyota";
+//   private model = "Corolla";
+//   public year = 2024;
+//   public displayDetails() {
+//     return `Brand ${this.brand} Model ${this.model} Year ${this.year}`;
+//   }
+// }
+// let myCar = new Car();
+// console.log(myCar.model); // Encapsulation
+
